b523ea42c1b773743e2d7608fb1bb3fc2766680a
sangminby <113090480+sangminby@users.noreply.github.com>
2022-10-24 16:19:35 +0900

    update

--- HallInOne/src/service/HioUpdateService.java
+++ HallInOne/src/service/HioUpdateService.java
@@ -1,5 +1,29 @@
 package service;
 
+import java.sql.SQLException;
+
+import util.HioConnection;
+
 public class HioUpdateService {
 
+	UpdateDAO Updatedao  = new HioUpdateDao();
+	
+	public int update(HioMember hiomember) {
+		
+		int result = 0;
+		HioConnection hioconn = null;
+		
+		try {
+			hioconn = HioConnection.getConnection();
+			
+			result = Updatedao.update(hioconn, hiomember);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	
 }
