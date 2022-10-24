0f11d7b32257df4897f1c9ccc47bee684c055ee2
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-24 21:36:22 +0900

    Update HioMemberDeleteDAO.java

--- HallInOne/src/hio/DAO/HioMemberDeleteDAO.java
+++ HallInOne/src/hio/DAO/HioMemberDeleteDAO.java
@@ -2,7 +2,10 @@ package hio.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
 
 import hio.domain.HioMember;
 
@@ -28,5 +31,48 @@ public class HioMemberDeleteDAO {
 
 		return result;
 	}
+	
+	public List<HioMember> selectToDelete(Connection conn) throws SQLException{
 
+		List<HioMember> allList = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+
+		try {
+			String sql = "select * from member";
+
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+
+			while (rs.next()) {
+
+				allList.add(new HioMember(rs.getInt("memberno"), rs.getString("membername"), 
+						rs.getString("memberaddres"), rs.getString("memberphone"), 
+						rs.getString("memberid"), rs.getString("memberpwd"), 
+						rs.getInt("membergrade")));
+			}	
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+
+		return allList;
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
