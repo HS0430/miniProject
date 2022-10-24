5e9ae269ad700335257459e2c68af855afb1129b
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-24 16:36:56 +0900

    Update HioDeleteController.java

--- HallInOne/src/hio/controller/HioDeleteController.java
+++ HallInOne/src/hio/controller/HioDeleteController.java
@@ -1,5 +1,28 @@
 package hio.controller;
 
-public class HioDeleteController implements HioDeleteInterface {
+import java.util.Scanner;
 
+import hio.DAO.OracleMemDeleteDao;
+import service.HioDeleteService;
+import service.HioInsertService;
+
+public class HioDeleteController implements HioDeleteInterface {
+	
+	Scanner sc = new Scanner(System.in);
+
+	@Override
+	public void memberDeletet() {
+
+		System.out.println("삭제할 회원 이름을 입력해주세요. >> ");
+		String delName = sc.nextLine().trim();
+
+		int result = new HioDeleteService().memberDelete(delName);
+		
+		if (result > 0) {
+			System.out.println("회원 삭제 완료!");
+		} else {
+			System.out.println("회원 삭제 실패!");
+		}
+
+	}
 }
