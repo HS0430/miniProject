1027887a2d9bc99d1c1b09f536078ccef01c8ca4
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-24 21:36:19 +0900

    Update HioMemberDeleteController.java

--- HallInOne/src/hio/controller/HioMemberDeleteController.java
+++ HallInOne/src/hio/controller/HioMemberDeleteController.java
@@ -1,7 +1,9 @@
 package hio.controller;
 
+import java.util.List;
 import java.util.Scanner;
 
+import hio.domain.HioMember;
 import service.HioMemberDeleteService;
 import service.HioInsertService;
 
@@ -12,6 +14,15 @@ public class HioMemberDeleteController implements HioMemberDeleteInterface {
 	@Override
 	public void memberDeletet() {
 
+		List<HioMember> allList = new HioMemberDeleteService().memberSelectToDelete();
+		
+		if (allList != null && !allList.isEmpty()) {
+			
+			for (HioMember h : allList) {
+				System.out.println(h);
+			}
+		}
+		
 		System.out.println("삭제할 회원 이름을 입력해주세요. >> ");
 		String delName = sc.nextLine().trim();
 
