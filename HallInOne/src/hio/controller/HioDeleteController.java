606b249394f70c7f7020839a7711d21752063eb3
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-24 17:13:25 +0900

    Update HioDeleteController.java

--- HallInOne/src/hio/controller/HioDeleteController.java
+++ HallInOne/src/hio/controller/HioDeleteController.java
@@ -24,4 +24,20 @@ public class HioDeleteController implements HioDeleteInterface {
 		}
 
 	}
+	
+	public void memberDeletet2() {
+
+		System.out.println("삭제할 회원 이름을 입력해주세요. >> ");
+		String delName = sc.nextLine().trim();
+
+		int result = new HioDeleteService().memberDelete(delName);
+		
+		if (result > 0) {
+			System.out.println("회원 삭제 완료!");
+		} else {
+			System.out.println("회원 삭제 실패!");
+		}
+
+	}
+	
 }
