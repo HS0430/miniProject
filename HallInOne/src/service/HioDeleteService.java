d5cd5839f7876215f42fe0aca046d53a0a9a8a02
skjung1979 <skjung1979.dvlp@gmail.com>
2022-10-24 16:36:49 +0900

    Update HioDeleteService.java

--- HallInOne/src/service/HioDeleteService.java
+++ HallInOne/src/service/HioDeleteService.java
@@ -1,5 +1,37 @@
 package service;
 
-public class HioDeleteService {
+import java.sql.Connection;
+import java.sql.SQLException;
+import hio.DAO.HioDeleteDAO;
+import util.HioConnection;
 
+public class HioDeleteService {
+	
+	HioDeleteDAO dao = new HioDeleteDAO();
+	
+	public int memberDelete(String delName) {
+		
+		int result = 0;
+		Connection conn = null;
+		
+		try {
+			
+			conn = HioConnection.getConnection();
+			result = dao.memberDelete(conn, delName);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return result;
+		
+	}
 }
