a51527675472c391ee9ac3d4b8dd78d3ec5fa8c2
HS0430 <hseok0430@gmail.com>
2022-10-25 09:53:22 +0900

    HioSelect > MemberLogin
    Select > MemberLogin으로 위치 변경

--- HallInOne/src/service/HioSelectService.java
+++ HallInOne/src/service/HioSelectService.java
@@ -9,31 +9,4 @@ import util.HioConnection;
 
 public class HioSelectService {
 	
-	HioSelectDAO dao = new HioSelectDAO();
-		
-	public int memberLogin(HioMember hioMember) {
-		
-		int result = 0;
-		Connection conn = null;
-
-		try {
-			conn = HioConnection.getConnection();
-			result = dao.memberLogin(conn, hioMember);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			try {
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
-		
-	}
 }
