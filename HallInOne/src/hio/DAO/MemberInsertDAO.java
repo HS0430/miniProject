6d6381e4423732044080b02594939dd181731fa9
HS0430 <hseok0430@gmail.com>
2022-10-25 12:21:00 +0900

    memberInsertDAO
    IsertDAO > memberInsertDAO 명칭 변경
    계정 가입시 id 중복여부 확인
    로그인 수정

--- HallInOne/src/hio/DAO/MemberInsertDAO.java
+++ HallInOne/src/hio/DAO/MemberInsertDAO.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import hio.domain.HioMember;
 
-public interface InsertDAO {
+public interface MemberInsertDAO {
 
 	public int memberInsert(Connection conn, HioMember hioMember) throws SQLException;
+	public int selectMemberName(Connection conn, String memberId) throws SQLException;
+	
 }
--- HallInOne/src/hio/controller/HioMemberLoginController.java
+++ HallInOne/src/hio/controller/HioMemberLoginController.java
@@ -15,18 +15,28 @@ public class HioMemberLoginController implements HioMemberLoginInterface{
 			
 			System.out.println("------------------------------------------------");
 			System.out.println("--------------------- 로그인 ---------------------");
-			System.out.print("회원 아이디 입력 >>");
-			String memberId = sc.nextLine();
-			System.out.print("회원 비밀번호 입력 >>");
-			String memberPwd = sc.nextLine();
-
 			
-			hioMember.setMemberId(memberId);
-			hioMember.setMemberPwd(memberPwd);
+			while(true) {
+				System.out.print("회원 아이디 입력 >>");
+				String memberId = sc.nextLine();
+				System.out.print("회원 비밀번호 입력 >>");
+				String memberPwd = sc.nextLine();
+	
+				
+				hioMember.setMemberId(memberId);
+				hioMember.setMemberPwd(memberPwd);
 			
-			result = new HioMemberLoginService().memberLogin(hioMember);
+				result = new HioMemberLoginService().memberLogin(hioMember);
+				
+				if(result < 0) {
+					System.out.println("로그인 실패 : 다시 시도해주세요");
+				}
+				else {
+					return result;
+				}
 			
-			return result;
+			}
+			
 	}
 	
 }
