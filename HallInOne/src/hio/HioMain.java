ed0777e1836f27ec740fa6c171fcc8b958dbbb0d
HS0430 <hseok0430@gmail.com>
2022-10-25 11:57:14 +0900

    HioMain 수정
    HioMain 수정

--- HallInOne/src/hio/HioMain.java
+++ HallInOne/src/hio/HioMain.java
@@ -41,6 +41,17 @@ public class HioMain {
 				}
 				else if(result == 0) {
 					hioAdminMenu();
+					int selectAdminNo = Integer.parseInt(sc.nextLine());
+					switch (selectAdminNo) {
+					case 1:
+						break;
+					case 2:
+						break;
+					case 3:
+						break;
+					case 4:
+						break;
+					}
 				}
 				break;
 			case 2:
